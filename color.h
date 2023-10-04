e2974bc6dfa64b21668af8bfceb2dd8fe61746af
abkhur <abkhur@vt.edu>
2023-10-03 21:59:19 -0400

    fixing color intensity

--- color.h
+++ color.h
@@ -7,6 +7,10 @@
 
 using color = vec3;
 
+inline double linear_to_gamma(double linear_component) {
+    return sqrt(linear_component);
+}
+
 void write_color(std::ostream &out, color pixel_color, int samples_per_pixel) {
     auto r = pixel_color.x();
     auto g = pixel_color.y();
@@ -17,6 +21,11 @@ void write_color(std::ostream &out, color pixel_color, int samples_per_pixel) {
     r*=scale;
     g*= scale;
     b*=scale;
+
+    //linear to gammafication
+    r = linear_to_gamma(r);
+    g = linear_to_gamma(g);
+    b = linear_to_gamma(b);
 
     //Writes [0,255] val of each color
     static const interval intensity(0.000, 0.999);
