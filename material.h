9b89ca0612ef55c06cd6482965b59b7d4a921ea6
abkhur <abkhur@vt.edu>
2023-10-03 22:38:22 -0400

    shitty approximation of glass

--- material.h
+++ material.h
@@ -36,18 +36,47 @@ class lambertian : public material {
 
 class metal : public material {
     public:
-        metal(const color& a) : albedo(a) {}
+        metal(const color& a, double f) : albedo(a), fuzz(f < 1 ? f : 1) {}
 
         bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered)
         const override {
             vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
-            scattered = ray(rec.p, reflected);
+            scattered = ray(rec.p, reflected + fuzz*random_unit_vector());
             attenuation = albedo;
-            return true;
+            return (dot(scattered.direction(), rec.normal) > 0);
         }
 
     private:
         color albedo;
+        double fuzz;
+};
+
+class dielectric : public material {
+    public:
+        dielectric(double index_of_refraction) : ir(index_of_refraction) {}
+
+        bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered) const override {
+            attenuation = color(1.0,1.0,1.0);
+            double refraction_ratio = rec.front_face ? (1.0/ir) : ir;
+
+            vec3 unit_direction = unit_vector(r_in.direction());
+            double cos_theta = fmin(dot(-unit_direction,rec.normal),1.0);
+            double sin_theta = sqrt(1.0 - cos_theta*cos_theta);
+            bool cannot_refract = refraction_ratio * sin_theta > 1.0;
+            vec3 direction;
+
+            if (cannot_refract) {
+                direction = reflect(unit_direction, rec.normal);
+            }
+            else {
+                direction = refract(unit_direction, rec.normal, refraction_ratio);
+            }
+            scattered = ray(rec.p, direction);
+            return true;
+        }
+
+    private:
+        double ir; //refraction index
 };
 
 #endif
